5d0fccea9dc5bdf5fd738db7293783698f2fda8a
Anowar Hossain <m.anowarbhai@gmail.com>
2018-10-28 16:33:32 +0600

    New

---
+++ js/main.js
@@ -0,0 +1,48 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+        $(".header-area").sticky({topSpacing: 0});
+            
+        $('li.smooth-menu a').bind('click', function(event) {
+            
+            var $anchor = $(this);
+            var headerH = '64';
+            $('html, body').stop().animate({
+                scrollTop : $($anchor.attr('href')).offset().top - headerH + "px"
+            }, 1200, 'easeInOutExpo');
+            
+            event.preventDefault();
+        });
+        
+        $('body').scrollspy({
+            
+            target: '.navbar-collapse',
+            offset: 95
+            });
+        
+        $('.parallax-bg, .top-area-bg').scrolly({bgParallax: true});
+
+        $('.navbar-toggle').click(function(){
+            $('body').addClass("mobile-menu-activated");
+        });
+        $('ul.nav.navbar-nav li a').click(function(){
+            $('.navbar-collapse').removeClass("in");
+        });
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
